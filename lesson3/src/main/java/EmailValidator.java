af2f85f4edd2827ae9afddef6d38f9bbcf58efa5
Dmitry501 <fedorov.dm501@gmail.com>
2019-04-20 21:19:29 +0300

    test

--- lesson3/src/main/java/EmailValidator.java
+++ lesson3/src/main/java/EmailValidator.java
@@ -1,9 +1,12 @@
 public class EmailValidator
 {
-  public static void main(String[] args)
-  {
-
-  }
+  /**
+   * Проверить что емейл соответствует формату username@companyname.domain
+   * - username, companyname  -- могут содержать только символы латинского алфавита, ".", "_", "-" и цифры
+   *  - domain -- может содержать только символы латинского алфавита
+   * @param email электронный адрес для проверки
+   * @return true если email валиден, false в противном случае проверка
+   */
   public boolean isValid(String email)
   {
     return true;
