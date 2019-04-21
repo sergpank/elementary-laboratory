c92c64cf21c5ca9c27d5286211861a0dfbb429b6
SeregaAteist <schvance@gmail.com>
2019-04-21 20:44:09 +0300

    COMPLETED

--- lesson3/src/main/java/MsisdnValidator.java
+++ lesson3/src/main/java/MsisdnValidator.java
@@ -1,12 +1,32 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Проверить что номер тлефона является валидным украинским мобильным номером
+ *
+ * @param msisdn Номер телефона формата +380-12-345-67-89
+ * @return true если номер валиден, false в противном случае
+ */
+
 public class MsisdnValidator
 {
-  /**
-   * Проверить что номер тлефона является валидным украинским мобильным номером
-   * @param msisdn Номер телефона формата +380-12-345-67-89
-   * @return true если номер валиден, false в противном случае
-   */
+  public static void main(String[] args)
+  {
+  }
+
   public boolean validate(String msisdn)
   {
-    return true;
+    String phoneNumber = "+380639962399";
+    Pattern pattern = Pattern.compile("^((\\+?380)([0-9]{9}))$");
+    Matcher matcher = pattern.matcher(phoneNumber);
+    if (matcher.matches())
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
 }
+
