bd854594ae8dbba5a359cab5571e9829ed453dee
SeregaAteist <schvance@gmail.com>
2019-04-21 19:48:45 +0300

    COMPLETED

--- lesson3/src/main/java/TwoSum.java
+++ lesson3/src/main/java/TwoSum.java
@@ -1,19 +1,26 @@
 public class TwoSum
 {
-    public int[] calcIndexes(int[] nums, int target) {
+  public int[] calcIndexes(int[] nums, int target)
+  {
     int complement;
 
-    for (int x = 0; x<nums.length; x++) {
+    for (int x = 0; x < nums.length; x++)
+    {
       complement = target - nums[x];
 
-      for (int y = 0; y<nums.length; y++) {
+      for (int y = 0; y < nums.length; y++)
+      {
 
-        if (x ==  y) { continue; }
-        if (nums[y] == complement) {
-          return new int[] {x, y};
+        if (x == y)
+        {
+          continue;
+        }
+        if (nums[y] == complement)
+        {
+          return new int[]{x, y};
         }
       }
     }
-    return new int[] {0, 0};
+    return new int[]{0, 0};
   }
 }
