ae8f819843d36b0d533907a765aaeb93db852748
SeregaAteist <schvance@gmail.com>
2019-04-21 18:32:17 +0300

    COMPLETED

--- lesson3/src/main/java/EmailValidator.java
+++ lesson3/src/main/java/EmailValidator.java
@@ -1,14 +1,36 @@
+/**
+ * Проверить что емейл соответствует формату username@companyname.domain
+ * - username, companyname  -- могут содержать только символы латинского алфавита, ".", "_", "-" и цифры
+ * - domain -- может содержать только символы латинского алфавита
+ *
+ * @param email электронный адрес для проверки
+ * @return true если email валиден, false в противном случае
+ */
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailValidator
 {
-  /**
-   * Проверить что емейл соответствует формату username@companyname.domain
-   * - username, companyname  -- могут содержать только символы латинского алфавита, ".", "_", "-" и цифры
-   *  - domain -- может содержать только символы латинского алфавита
-   * @param email электронный адрес для проверки
-   * @return true если email валиден, false в противном случае
-   */
+  public static void main(String[] args)
+  {
+
+  }
+
+  private static final String EMAIL_REGEX = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
+
+  private static Pattern pattern;
+  private Matcher matcher;
+
+  public EmailValidator()
+  {
+
+    pattern = Pattern.compile(EMAIL_REGEX, Pattern.CASE_INSENSITIVE);
+  }
+
   public boolean isValid(String email)
   {
-    return true;
+    matcher = pattern.matcher(email);
+    return this.matcher.matches();
   }
 }
