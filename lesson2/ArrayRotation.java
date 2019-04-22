4536ee0febf5d24fe2f0674aec2de163da446702
SeregaAteist <schvance@gmail.com>
2019-04-22 11:59:35 +0300

    +++

---
+++ lesson2/ArrayRotation.java
@@ -0,0 +1,47 @@
+public class ArrayRotation
+{
+  public static void main(String[] argv)
+  {
+    int m = 3;
+    int[][] A = new int[m][m];
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < m; j++)
+      {
+        A[i][j] = m * i + j;
+      }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < m; j++)
+      {
+        System.out.printf("%3d ", A[i][j]);
+      }
+      System.out.print("\n");
+    }
+    System.out.print("\nШАЗАМ:\n\n");
+
+    for (int k = 0; k < m / 2; k++)
+    {
+      for (int j = k; j < m - 1 - k; j++)
+      {
+
+        int tmp = A[k][j];
+        A[k][j] = A[j][m - 1 - k];
+        A[j][m - 1 - k] = A[m - 1 - k][m - 1 - j];
+        A[m - 1 - k][m - 1 - j] = A[m - 1 - j][k];
+        A[m - 1 - j][k] = tmp;
+      }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < m; j++)
+      {
+        System.out.printf("%3d ", A[i][j]);
+      }
+      System.out.print("\n");
+    }
+  }
+}
