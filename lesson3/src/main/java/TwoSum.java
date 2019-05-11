fe627b667cad9cdbc981843e72c4534315f5220b
Arthur <kutusenko@meta.ua>
2019-05-11 22:19:04 +0300

    new

--- lesson3/src/main/java/TwoSum.java
+++ lesson3/src/main/java/TwoSum.java
@@ -1,7 +1,24 @@
 public class TwoSum
 {
-  public int[] calcIndexes(int[] nums, int target)
+  public int[] calcIndexes(int[] mass, int target)
   {
+    for (int i = 0; i < mass.length - 1; ++i)
+    {
+      for (int j = i + 1; j < mass.length; ++j)
+      {
+        if (mass[i] == mass[j])
+        {
+          continue;
+        }
+        int sum = mass[i] + mass[j];
+        if (sum == target)
+        {
+          return new int[]{i, j};
+        }
+      }
+    }
+
+
     return new int[]{};
   }
 }
