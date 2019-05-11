9e22974306dffbc8688a2446137898c6f7aa6250
Arthur <kutusenko@meta.ua>
2019-05-11 22:26:31 +0300

    new

--- lesson3/src/main/java/EmailValidator.java
+++ lesson3/src/main/java/EmailValidator.java
@@ -1,14 +1,20 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailValidator
 {
   /**
    * Проверить что емейл соответствует формату username@companyname.domain
    * - username, companyname  -- могут содержать только символы латинского алфавита, ".", "_", "-" и цифры
-   *  - domain -- может содержать только символы латинского алфавита
+   * - domain -- может содержать только символы латинского алфавита
+   *
    * @param email электронный адрес для проверки
    * @return true если email валиден, false в противном случае
    */
   public boolean isValid(String email)
   {
-    return true;
+    Pattern pat = Pattern.compile("[a-z[0-9][/./_/-]]+@[a-z[0-9][/./_/-]]+.[a-z]+");
+    Matcher mat= pat.matcher(email);
+    return mat.matches();
   }
 }
