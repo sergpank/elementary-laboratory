6eecc382f3eb4db26f3e0e7022667bf5c3209c03
kshilov <kirpichk@gmail.com>
2019-05-14 15:17:52 +0300

    solution of the problem LinkedList

--- lesson6/src/main/java/list/LinkedList.java
+++ lesson6/src/main/java/list/LinkedList.java
@@ -3,35 +3,90 @@ package list;
 public class LinkedList
 {
   private int size;
+  private Element root;
 
   private class Element
   {
     private Element next;
     private Integer data;
+
+    public Element(Integer data)
+    {
+      this.data = data;
+    }
   }
 
   public void add(Integer data)
   {
+    if (size == 0)
+    {
+      root = new Element(data);
+      size++;
+    }
+    else
+    {
+      Element current = root;
 
+      while (current.next != null)
+      {
+        current = current.next;
+      }
+      current.next = new Element(data);
+      size++;
+    }
   }
 
   public Integer get(int index)
   {
-
+    if (size == 0)
+    {
+      throw new NullPointerException();
+    }
+    if (index < 0 || index >= size)
+    {
+      throw new ArrayIndexOutOfBoundsException();
+    }
+    Element current = root;
+    for (int i = 0; i != index; i++)
+    {
+      current = current.next;
+    }
+    return current.data;
   }
 
   /**
    * Удалить элемент из списка
+   *
    * @param index Индекс удаляемого элемента
    * @return значение элемента
    */
   public Integer remove(int index)
   {
-
+    if (size == 0)
+    {
+      throw new NullPointerException();
+    }
+    if (index < 0 || index >= size)
+    {
+      throw new ArrayIndexOutOfBoundsException();
+    }
+    Element current = root;
+    Element prev = current;
+    for (int i = 0; i != index; i++)
+    {
+      prev = current;
+      current = current.next;
+    }
+    if (current.next != null)
+    {
+      prev.next = current.next;
+      size--;
+    }
+    return current.data;
   }
 
   public int size()
   {
-
+    return size;
   }
 }
