3dab9d330f2375c725838fce46992cb3fbec51c0
Chugunov <b.chugunov75@gmail.com>
2019-05-16 03:54:04 +0300

    homework 20

---
+++ lesson6/src/main/java/homework20/LinkedList.java
@@ -0,0 +1,103 @@
+package homework20;
+
+public class LinkedList<T>
+{
+    private int size;
+    private Element<T> first;
+    private Element<T> last;
+
+    private static class Element<T>
+    {
+        private Element<T> next;
+        private T data;
+
+        Element(T data)
+        {
+            this.data=data;
+        }
+    }
+
+    public void add(T data)
+    {
+        @SuppressWarnings("unchecked")
+        Element<T> node=(Element<T>) new Element(data);
+
+        if(last==null)
+        {
+            first=node;
+            last=node;
+        }
+        else
+        {
+            last.next=node;
+            last=node;
+        }
+        size++;
+    }
+
+    public T get(int index)
+    {
+        Element<T> node=first;
+
+        for(int i=1; i<=index && node!=null; i++)
+        {
+            node=node.next;
+        }
+
+        return (node!=null)? node.data : null;
+    }
+
+    public T getFirst()
+    {
+        Element<T> node=first;
+
+        return (node!=null)? node.data : null;
+    }
+
+    public T getLast()
+    {
+        Element<T> node=last;
+
+        return (node!=null)? node.data : null;
+    }
+
+    public T remove(int index)
+    {
+        Element<T> node=first;
+        Element<T> prevNode=null;
+        T removed=null;
+
+        for(int i=1; i<=index && node!=null; i++)
+        {
+            prevNode=node;
+            node=node.next;
+        }
+
+        if(node!=null)
+        {
+            removed=node.data;
+            
+            if(first==node)
+            {
+                first=node.next;
+            }
+            else
+            {
+                prevNode.next=node.next;
+            }
+            if(node==last)
+            {
+                last=prevNode;
+            }
+
+            size--;
+        }
+
+        return removed;
+    }
+
+    public int size()
+    {
+        return size;
+    }
+}
---
+++ lesson6/src/test/java/homework20/LinkedListTest.java
@@ -0,0 +1,45 @@
+package homework20;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LinkedListTest
+{
+    @Test
+    public void test1()
+    {
+        LinkedList list = new LinkedList();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        assertEquals(5, list.size());
+
+        assertEquals(Integer.valueOf(1), list.getFirst());
+        assertEquals(Integer.valueOf(5), list.getLast());
+
+        assertEquals(Integer.valueOf(1), list.get(0));
+        assertEquals(Integer.valueOf(2), list.get(1));
+        assertEquals(Integer.valueOf(3), list.get(2));
+
+        list.remove(0);
+        assertEquals(4, list.size());
+        assertEquals(Integer.valueOf(2), list.getFirst());
+        assertEquals(Integer.valueOf(4), list.get(2));
+
+        list.remove(3);
+        assertEquals(3, list.size());
+        assertEquals(Integer.valueOf(4), list.getLast());
+        assertEquals(Integer.valueOf(4), list.get(2));
+
+        assertNull(list.remove(3));
+        assertEquals(3, list.size());
+
+        assertNull(list.get(3));
+    }
+
+}
