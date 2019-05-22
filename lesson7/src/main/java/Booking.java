fca423e03a1922fc62dfcde3a424c4c295c46f07
alexzhuravlov <alexzhuravlov13@gmail.com>
2019-05-22 19:37:51 +0300

    lesson 7 comparator hotels

--- lesson7/src/main/java/Booking.java
+++ lesson7/src/main/java/Booking.java
@@ -4,39 +4,49 @@ public class Booking
 {
   public static void main(String[] args)
   {
-    Set<String> keys = new HashSet<>();
-    keys.add("breakfast");
-    keys.add("beach");
-    keys.add("citycenter");
-    keys.add("location");
-    keys.add("metro");
-    keys.add("view");
-    keys.add("staff");
-    keys.add("price");
+    Set<String> SearchKeys = new HashSet<>();
+    SearchKeys.add("breakfast");
+    SearchKeys.add("beach");
+    SearchKeys.add("citycenter");
+    SearchKeys.add("location");
+    SearchKeys.add("metro");
+    SearchKeys.add("view");
+    SearchKeys.add("staff");
+    SearchKeys.add("price");
 
-    Map<Integer, List<String>> reviewMap = createData();
+    Map<Integer, List<String>> ReviewsMap = BookingData();
+    Rank RankCalculator = new Rank ();
+
+    Map<Integer, Integer>  HotelsRank = RankCalculator.RankCalculate ( ReviewsMap, SearchKeys );
+    Map<Integer, Integer> SortedRanks = RankCalculator.SortbyRank ( HotelsRank );
+
+    System.out.println (SortedRanks );
+
+
+    for(Map.Entry<Integer, Integer> e : SortedRanks.entrySet())
+    {
+      System.out.printf("Rank %d : Hotel id %s\n", e.getKey(), e.getValue());
+    }
+
 
   }
 
-  private static Map<Integer, List<String>> createData()
+  private static Map<Integer, List<String>> BookingData()
   {
-    Map<Integer, List<String>> reviewMap = new HashMap<>();
+    Map<Integer, List<String>> ReviewsMap = new HashMap<>();
 
     List<String> hotel1Reviews = new ArrayList<>();
-    hotel1Reviews.add("The breakfast is ok. Regarding location, it is quite fat from the citycenter. But price is cheap, so it is ok.");
-    hotel1Reviews.add("Very friendly staff and good cost-benefit ratio. Its location is a bit far from citycenter");
+    hotel1Reviews.add("This hotel has a nice view of the citycenter. The location is perfect.");
+    hotel1Reviews.add("Location is excellent, 5 min from citycenter. There is also a metro station pretty close to the hotel.");
+    hotel1Reviews.add("They said I couldn't take my dog. But there were other guests with dogs! That is not fair.");
 
     List<String> hotel2Reviews = new ArrayList<>();
-    hotel2Reviews.add("This hotel has a nice view of the citycenter. The location is perfect.");
-    hotel2Reviews.add("Location is excellent, 5 min from citycenter. There is also a metro station pretty close to the hotel.");
-    hotel2Reviews.add("They said I couldn't take my dog. But there were other guests with dogs! That is not fair.");
+    hotel2Reviews.add("The breakfast is ok. Regarding location, it is quite fat from the citycenter. But price is cheap, so it is ok.");
+    hotel2Reviews.add("Very friendly staff and good cost-benefit ratio. Its location is a bit far from citycenter");
 
-    List<String> hotel3Reviews = new ArrayList<>();
-    hotel3Reviews.add("price price price price price price");
 
-    reviewMap.put(1, hotel1Reviews);
-    reviewMap.put(2, hotel2Reviews);
-    reviewMap.put(3, hotel3Reviews);
-    return reviewMap;
+    ReviewsMap.put(1, hotel1Reviews);
+    ReviewsMap.put(2, hotel2Reviews);
+    return ReviewsMap;
   }
 }
--- lesson7/src/main/java/Rank.java
+++ lesson7/src/main/java/Rank.java
@@ -1,3 +1,64 @@
+import java.util.*;
+
 public class Rank
 {
+  public Map RankCalculate(Map<Integer, List<String>> ReviewsMap , Set<String> SearchKeys)
+  {
+    Map<Integer, Integer> IdtoRank = new HashMap<> ( ); //создали карту для наших айди и оценки
+    for ( Map.Entry<Integer, List<String>> entry : ReviewsMap.entrySet ( ) ) //получаем значение ключа и значения из нашей карты
+    {
+      int hotelId = entry.getKey ( ); //номера отелей записали, потом они будут в новой мапе
+      for ( String review : entry.getValue ( ) ) //вытянули значения
+      {
+        String[] ReviewtoWords = review.toLowerCase ( ).split ( "[ \\.,!]" ); //заполняем массив словами, разделительными знаками и пробелом (по каждой строке и разбиваем, так все строки)
+        for ( String word : ReviewtoWords ) //идём по массиву слов
+        {
+          for ( String key : SearchKeys ) //идём по списку ключевых слов
+          {
+            if (word.contains ( key ))
+            {
+              Integer rank = IdtoRank.get ( hotelId );
+              if (rank == null)
+              {
+                rank = 0;
+              }
+
+              IdtoRank.put ( hotelId , rank + 1 );
+            }
+          }
+        }
+      }
+    }
+
+
+
+
+
+    return IdtoRank;
+  }
+
+
+  public Map SortbyRank(Map<Integer, Integer> IdtoRank)
+  {
+    Map<Integer, Integer> RanktoId = new TreeMap<> ( new Comparator<Integer> ( )
+    {
+      @Override
+      public int compare(Integer rank1 , Integer rank2)
+      {
+        return rank2-rank1;
+      }
+    } );
+
+    for ( Integer id : IdtoRank.keySet ( ) )
+    {
+      int rank = IdtoRank.get ( id );
+      RanktoId.put ( rank , id );
+
+
+  }
+
+    return RanktoId;
+}
+
+
 }
