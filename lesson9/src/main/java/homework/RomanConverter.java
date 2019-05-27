8d2df79be916abed3973285c74bdab052b186677
Roman Velikorod <romavelik@gmail.com>
2019-05-27 13:18:27 +0300

    RomanConverter solution

--- lesson9/src/main/java/homework/RomanConverter.java
+++ lesson9/src/main/java/homework/RomanConverter.java
@@ -1,5 +1,6 @@
 package homework;
 
+
 public class RomanConverter
 {
   public String toRoman(int i)
@@ -31,7 +32,67 @@ public class RomanConverter
     if (i == 5)
     {
       sb.append("V");
-      toRoman(i - 5, sb);
+      toRoman(i - 5,sb);
+    }
+    if(i>5&&i<9)
+    {
+      sb.append("VI");
+      toRoman(i-6, sb);
+    }
+    if(i==9)
+    {
+      sb.append("I");
+      toRoman(i+1,sb);
+    }
+    if (i == 10)
+    {
+      sb.append("X");
+      toRoman(i-10,sb);
+    }
+    if(i>10&&i<40)
+    {
+      sb.append("X");
+      toRoman(i-10,sb);
+    }
+    if(i>=40&&i<50)
+    {
+      sb.append("XL");
+      toRoman(i-40,sb);
+    }
+    if(i>=50&&i<90)
+    {
+      sb.append("L");
+      toRoman(i-50, sb);
+    }
+    if (i>=90&&i<100)
+    {
+      sb.append("XC");
+      toRoman(i-90,sb);
+    }
+    if(i>=100&&i<400)
+    {
+      sb.append("C");
+      toRoman(i-100,sb);
+    }
+    if(i>=400&&i<500)
+    {
+      sb.append("CD");
+      toRoman(i-400, sb);
+    }
+    if (i>=500&&i<900)
+    {
+      sb.append("D");
+      toRoman(i-500,sb);
+    }
+    if(i>=900&&i<1000)
+    {
+      sb.append("CM");
+      toRoman(i-900,sb);
+    }
+    if (i>=1000&&i<4000)
+    {
+      sb.append("M");
+      toRoman(i-1000, sb);
     }
   }
 }
--- lesson9/src/test/java/homework/RomanConverterTest.java
+++ lesson9/src/test/java/homework/RomanConverterTest.java
@@ -59,4 +59,278 @@ public class RomanConverterTest
 
     Assert.assertEquals(expected, actual);
   }
+
+  @Test
+  public void test6()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(6);
+    String expected = "VI";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test7()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(7);
+    String expected = "VII";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test8()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(8);
+    String expected = "VIII";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test9()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(9);
+    String expected = "IX";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test10()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(10);
+    String expected = "X";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test11()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(11);
+    String expected = "XI";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test12()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(20);
+    String expected = "XX";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test13()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(30);
+    String expected = "XXX";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test14()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(40);
+    String expected = "XL";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test15()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(50);
+    String expected = "L";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test16()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(60);
+    String expected = "LX";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test17()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(70);
+    String expected = "LXX";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test18()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(80);
+    String expected = "LXXX";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test19()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(90);
+    String expected = "XC";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test20()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(100);
+    String expected = "C";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test21()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(399);
+    String expected = "CCCXCIX";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test22()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(400);
+    String expected = "CD";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test23()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(401);
+    String expected = "CDI";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test24()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(499);
+    String expected = "CDXCIX";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test25()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(500);
+    String expected = "D";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test26()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(501);
+    String expected = "DI";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test27()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(899);
+    String expected = "DCCCXCIX";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test28()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(900);
+    String expected = "CM";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test29()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(901);
+    String expected = "CMI";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test30()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(999);
+    String expected = "CMXCIX";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test31()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(1000);
+    String expected = "M";
+
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void test32()
+  {
+    RomanConverter converter = new RomanConverter();
+
+    String actual = converter.toRoman(3999);
+    String expected = "MMMCMXCIX";
+
+    Assert.assertEquals(expected, actual);
+  }
+
+
+
 }
