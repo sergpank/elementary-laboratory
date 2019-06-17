4374b353618ffd9052ee163e13d9092c979364d1
sergpank <sergpank@gmail.com>
2019-06-17 20:43:50 +0300

    lab 14 init

---
+++ lesson14-Swing-FactoryMethod-AbstractFactory/src/main/java/SwingDemo.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class SwingDemo
+{
+  public static void main(String[] args)
+  {
+    MainFrame mainFrame = new MainFrame();
+
+    SwingUtilities.invokeLater(new Runnable()
+    {
+      @Override
+      public void run()
+      {
+        mainFrame.initUI();
+      }
+    });
+  }
+}
