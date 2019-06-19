d580c8ab2d00b15ef91a37934d033aeded141da6
Dmitry501 <fedorov.dm501@gmail.com>
2019-06-19 17:57:23 +0000

    1

---
+++ lesson14-Swing-FactoryMethod-AbstractFactory/src/main/java/fedorov/MainFrame.java
@@ -0,0 +1,103 @@
+package fedorov;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame extends JFrame
+{
+  private static final Logger log = LogManager.getLogger(MainFrame.class);
+
+  public void initUI()
+  {
+    this.setTitle("Super cool DB client");
+    this.setLayout(new BorderLayout());
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JPanel connectionPanel = createConnectionPanel();
+    JPanel tableListPanel = createTableListPanel();
+    JPanel dataTable = createMockTablePanel();
+
+    this.add(connectionPanel, BorderLayout.NORTH);
+    this.add(tableListPanel, BorderLayout.WEST);
+    this.add(dataTable, BorderLayout.CENTER);
+
+    this.pack();
+    this.setSize(960, 640);
+//    setResizable(false);
+    this.setVisible(true);
+
+    log.info("UI is initialized");
+  }
+
+  private JPanel createConnectionPanel()
+  {
+    JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+
+    final JLabel label = new JLabel("JDBC URL : ");
+    panel.add(label, BorderLayout.WEST);
+
+    final JTextField pathField = new JTextField("insert jdbc path here", 32);
+    panel.add(pathField, BorderLayout.NORTH);
+
+    JButton openButton = new JButton("Open");
+    panel.add(openButton, BorderLayout.EAST);
+
+    openButton.addActionListener(new ActionListener()
+    {
+      @Override
+      public void actionPerformed(ActionEvent e)
+      {
+        JOptionPane.showMessageDialog(panel, "HELLO !");
+      }
+    });
+
+    return panel;
+  }
+
+  private JPanel createTableListPanel()
+  {
+    JPanel tableListPanel = new JPanel();
+    tableListPanel.setLayout(new BoxLayout(tableListPanel, BoxLayout.Y_AXIS));
+
+    tableListPanel.add(new JLabel("Table_01"));
+    tableListPanel.add(new JLabel("Table_02"));
+    tableListPanel.add(new JLabel("Table_03"));
+    tableListPanel.add(new JLabel("Table_04"));
+    tableListPanel.add(new JLabel("Table_05"));
+    tableListPanel.add(new JLabel("Table_06"));
+    tableListPanel.add(new JLabel("Table_07"));
+
+    final JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+    panel.add(tableListPanel);
+    return panel;
+  }
+
+  private JPanel createMockTablePanel()
+  {
+    String[] columnNames = new String[] {"Column A", "Column B", "Column C"};
+    String[][] rowData = new String[][]{
+        {"cell 1", "cell 2", "cell 3"},
+        {"cell 1", "cell 2", "cell 3"},
+        {"cell 1", "cell 2", "cell 3"},
+        {"cell 1", "cell 2", "cell 3"},
+        {"cell 1", "cell 2", "cell 3"},
+        {"cell 1", "cell 2", "cell 3"},
+        {"cell 1", "cell 2", "cell 3"},
+        {"cell 1", "cell 2", "cell 3"},
+        {"cell 1", "cell 2", "cell 3"},
+        {"cell 1", "cell 2", "cell 3"}
+    };
+
+    JTable table = new JTable(rowData, columnNames);
+
+    JPanel panel = new JPanel(new BorderLayout(10, 10));
+    panel.add(new JScrollPane(table), BorderLayout.CENTER);
+
+    return panel;
+  }
+}
---
+++ lesson14-Swing-FactoryMethod-AbstractFactory/src/main/java/fedorov/SwingDemo.java
@@ -0,0 +1,27 @@
+package fedorov;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.swing.*;
+
+public class SwingDemo
+{
+  private static final Logger log = LogManager.getLogger(SwingDemo.class);
+
+  public static void main(String[] args)
+  {
+    log.debug("Starting application ...");
+    MainFrame mainFrame = new MainFrame();
+
+    SwingUtilities.invokeLater(new Runnable()
+    {
+      @Override
+      public void run()
+      {
+        mainFrame.initUI();
+      }
+    });
+  }
+}
