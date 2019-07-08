d13a7f6952f8d4edac2b11a7489578feefb1b57c
sergpank <sergpank@gmail.com>
2019-07-08 23:17:38 +0300

    lab19 - POWA

---
+++ lesson19-POWA/src/main/java/com/elementary/forum/MAIN.java
@@ -0,0 +1,39 @@
+package com.elementary.forum;
+
+import com.elementary.forum.dao.TopicDao;
+import com.elementary.forum.dao.UserDao;
+import com.elementary.forum.entites.Topic;
+import com.elementary.forum.entites.User;
+
+import java.util.Date;
+
+public class MAIN
+{
+  public static void main(String[] args)
+  {
+    UserDao userDao = new UserDao();
+    TopicDao topicDao = new TopicDao();
+
+    for (int i = 1; i <=5; i++)
+    {
+      User user = new User();
+      user.setLogin("user-" + i);
+      user.setPassword("password-" + i);
+      user.setRegistrationDate(new Date());
+
+      userDao.save(user);
+    }
+
+    for (int i = 1; i <=5; i++)
+    {
+      Topic topic = new Topic();
+      topic.setAuthor(userDao.getById(i));
+      topic.setDateCreated(new Date());
+      topic.setTitle("Topic-" + i);
+
+      topicDao.save(topic);
+    }
+
+    topicDao.loadAll().forEach(System.out::println);
+  }
+}
