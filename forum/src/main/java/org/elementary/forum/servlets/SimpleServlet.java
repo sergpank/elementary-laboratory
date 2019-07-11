d76d019a9aa8eb2137abe0c760f65fea5d7135fd
Roman <romavelik@gmail.com>
2019-07-11 22:09:00 +0300

    forum

---
+++ forum/src/main/java/org/elementary/forum/servlets/SimpleServlet.java
@@ -0,0 +1,20 @@
+package org.elementary.forum.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class SimpleServlet  extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("Hi from servlet! mv" + new Date());
+        out.flush();
+        out.close();
+    }
+}
