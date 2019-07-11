c7f6f6931d7bc05cc93c49477049d48071a6f8e5
Илья Демков <zigidrafin1606@gmail.com>
2019-07-11 21:05:48 +0300

    dd

--- lesson7/src/main/java/RankCalculator.java
+++ lesson7/src/main/java/RankCalculator.java
@@ -2,50 +2,118 @@ import java.util.*;
 
 public class RankCalculator
 {
-  public Map<Integer, Set<Integer>> calculateRank(Set<String> keys, Map<Integer, List<String>> reviewMap)
-  {
-    Map<Integer, Integer> idToRankMap = new HashMap<>();
+  private Map<Integer, List<String>> ReviewsMap = Reviews();
+  private Set<String> keys = new HashSet<>();
 
-    for (Map.Entry<Integer, List<String>> entry : reviewMap.entrySet())
+  {
+    keys.add("breakfast");
+    keys.add("beach");
+    keys.add("citycenter");
+    keys.add("location");
+    keys.add("metro");
+    keys.add("view");
+    keys.add("staff");
+    keys.add("price");
+  }
+
+
+  private Map Reviews()
+  {
+    Map<Integer, List<String>> ReviewsMap = new HashMap<>();
+
+    List<String> hotel1Reviews = new ArrayList<>();
+    hotel1Reviews.add("This hotel has a nice view of the citycenter. The location is perfect.");
+    hotel1Reviews.add("Location is excellent, 5 min from citycenter. There is also a metro station pretty close to the hotel.");
+    hotel1Reviews.add("They said I couldn't take my dog. But there were other guests with dogs! That is not fair.");
+
+    List<String> hotel2Reviews = new ArrayList<>();
+    hotel2Reviews.add("The breakfast is ok. Regarding location, it is quite fat from the citycenter. But price is cheap, so it is ok.");
+    hotel2Reviews.add("Very friendly staff and good cost-benefit ratio. Its location is a bit far from citycenter");
+
+
+    ReviewsMap.put(1, hotel1Reviews);
+    ReviewsMap.put(2, hotel2Reviews);
+    return ReviewsMap;
+  }
+
+
+  public static void main(String[] args)
+  {
+    RankCalculator rank = new RankCalculator();
+
+    System.out.println(rank.Relevant());
+
+  }
+
+  private Map Relevant()
+  {
+    Map<Integer, Integer> rankMap = new HashMap<>();
+
+    for (Map.Entry<Integer, List<String>> entry : ReviewsMap.entrySet())
     {
-      Integer hotelId = entry.getKey();
+      Integer hotelID = entry.getKey();
 
       for (String review : entry.getValue())
       {
-        String[] tokens = review.toLowerCase().split("[ \\.,!]");
-
-        for (String token : tokens)
+        String[] splitReview = review.split("[\\s\\W]");
+        for (String words : splitReview)
         {
-          if (keys.contains(token))
-          {
-            Integer rank = idToRankMap.get(hotelId);
 
+          if (keys.contains(words))
+          {
+
+            Integer rank = rankMap.get(hotelID);
             if (rank == null)
             {
               rank = 0;
             }
+            rankMap.put(hotelID, rank + 1);
 
-            idToRankMap.put(hotelId, rank + 1);
           }
+
         }
+
+
       }
+
     }
 
-    return sortByRank(idToRankMap);
+    return sortByRank(rankMap);
+
   }
 
-  private Map<Integer, Set<Integer>> sortByRank(Map<Integer, Integer> idToRankMap)
+
+  private Map<Integer, Set<Integer>> sortByRank(Map<Integer, Integer> rankMap)
   {
-    Map<Integer, Set<Integer>> rankToIdMap = new TreeMap<>();
-
-    for (Integer id : idToRankMap.keySet())
+    Map<Integer, Set<Integer>> sortedMap = new TreeMap<>(new Comparator<Integer>()
     {
-      Integer rank = idToRankMap.get(id);
+      @Override
+      public int compare(Integer o1, Integer o2)
+      {
+        return o2 - o1;
+      }
+    });
 
-      Set<Integer> ids = rankToIdMap.computeIfAbsent(rank, k -> new TreeSet<>());
+    for (Integer id : rankMap.keySet())
+    {
+      Integer rank = rankMap.get(id);
+
+      Set<Integer> ids = sortedMap.get(rank);
+
+      if (ids == null)
+      {
+        ids = new TreeSet<>();
+
+      }
+
+      sortedMap.put(rank, ids);
+
       ids.add(id);
+
     }
 
-    return rankToIdMap;
+    return sortedMap;
+
+
   }
 }
